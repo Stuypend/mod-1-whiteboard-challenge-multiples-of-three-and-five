315d609ec30d0b3ade94a6ca0734890b66cc137b
stuypend <andre@stuypend.com>
2021-03-11 09:38:05 -0500

    commit tree 1097c25645f7fb0c291380946503dbfdd38377d8

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,13 @@
-# Enter your procedural solution here!
+def euler(num)
+  count = 1
+  until(count > num)
+    if((count*3) < num)
+      sum += (count*3)
+    end
+    if((count*5) < num)
+      sum += (count*5)
+    end
+    count +=1
+  end
+  return sum
+end
